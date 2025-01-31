62b05f3badbe154e70b8e2ef10c969b32c7b7101
neuleui <sbsb840516@gmail.com>
2025-01-31 09:27:20 +0900

    Update json_Car.sql

--- json_Car.sql
+++ json_Car.sql
@@ -1,3 +1,4 @@
+//CAR
 INSERT INTO Car (Car_ID, jsonstr) VALUES ('CAR001', '{"Name": "Avante", "Type": "Sedan", "Option": "B"}');
 INSERT INTO Car (Car_ID, jsonstr) VALUES ('CAR001', '{"Name": "Avante", "Type": "Sedan", "Option": "P"}');
 INSERT INTO Car (Car_ID, jsonstr) VALUES ('CAR002', '{"Name": "Sonata", "Type": "Sedan", "Option": "B"}');
@@ -32,3 +33,30 @@ INSERT INTO Car (Car_ID, jsonstr) VALUES ('CAR016', '{"Name": "Ioniq 6", "Type":
 INSERT INTO Car (Car_ID, jsonstr) VALUES ('CAR016', '{"Name": "Ioniq 6", "Type": "Electric", "Option": "P"}');
 INSERT INTO Car (Car_ID, jsonstr) VALUES ('CAR017', '{"Name": "Ioniq 5", "Type": "Electric", "Option": "B"}');
 INSERT INTO Car (Car_ID, jsonstr) VALUES ('CAR017', '{"Name": "Ioniq 5", "Type": "Electric", "Option": "P"}');
+
+
+
+//Employee
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E001', '{"Emp_Name": "박민수", "Email_Address": "employee1@hyundai.com", "Emp_Pwd": "1111", "Emp_Phone": "010-5555-1001", "Position": "부장", "Branch_ID": "'B01'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E002', '{"Emp_Name": "한지훈", "Email_Address": "employee2@hyundai.com", "Emp_Pwd": "2222", "Emp_Phone": "010-5555-1002", "Position": "차장", "Branch_ID": "'B01'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E003', '{"Emp_Name": "이수민", "Email_Address": "employee3@hyundai.com", "Emp_Pwd": "3333", "Emp_Phone": "010-5555-1003", "Position": "과장", "Branch_ID": "'B01'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E004', '{"Emp_Name": "김정우", "Email_Address": "employee4@hyundai.com", "Emp_Pwd": "4444", "Emp_Phone": "010-5555-1004", "Position": "과장", "Branch_ID": "'B01'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E005', '{"Emp_Name": "송현지", "Email_Address": "employee5@hyundai.com", "Emp_Pwd": "5555", "Emp_Phone": "010-5555-1005", "Position": "대리", "Branch_ID": "'B01'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E006', '{"Emp_Name": "최동훈", "Email_Address": "employee6@hyundai.com", "Emp_Pwd": "6666", "Emp_Phone": "010-5555-1006", "Position": "대리", "Branch_ID": "'B01'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E007', '{"Emp_Name": "서하연", "Email_Address": "employee7@hyundai.com", "Emp_Pwd": "7777", "Emp_Phone": "010-5555-1007", "Position": "주임", "Branch_ID": "'B01'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E008', '{"Emp_Name": "강민혁", "Email_Address": "employee8@hyundai.com", "Emp_Pwd": "8888", "Emp_Phone": "010-5555-1008", "Position": "주임", "Branch_ID": "'B01'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E009', '{"Emp_Name": "윤지수", "Email_Address": "employee9@hyundai.com", "Emp_Pwd": "9999", "Emp_Phone": "010-5555-1009", "Position": "사원", "Branch_ID": "'B01'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E010', '{"Emp_Name": "배진우", "Email_Address": "employee10@hyundai.com", "Emp_Pwd": "1010", "Emp_Phone": "010-5555-1010", "Position": "차장", "Branch_ID": "'B02'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E011', '{"Emp_Name": "이하준", "Email_Address": "employee11@hyundai.com", "Emp_Pwd": "11111", "Emp_Phone": "010-5555-1011", "Position": "과장", "Branch_ID": "'B02'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E012', '{"Emp_Name": "전수현", "Email_Address": "employee12@hyundai.com", "Emp_Pwd": "1212", "Emp_Phone": "010-5555-1012", "Position": "대리", "Branch_ID": "'B02'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E013', '{"Emp_Name": "임지호", "Email_Address": "employee13@hyundai.com", "Emp_Pwd": "1313", "Emp_Phone": "010-5555-1013", "Position": "주임", "Branch_ID": "'B02'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E014', '{"Emp_Name": "강주원", "Email_Address": "employee14@hyundai.com", "Emp_Pwd": "1414", "Emp_Phone": "010-5555-1014", "Position": "사원", "Branch_ID": "'B02'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E015', '{"Emp_Name": "문지현", "Email_Address": "employee15@hyundai.com", "Emp_Pwd": "1515", "Emp_Phone": "010-5555-1015", "Position": "사원", "Branch_ID": "'B02'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E016', '{"Emp_Name": "한서윤", "Email_Address": "employee16@hyundai.com", "Emp_Pwd": "1616", "Emp_Phone": "010-5555-1016", "Position": "부장", "Branch_ID": "'B03'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E017', '{"Emp_Name": "정우진", "Email_Address": "employee17@hyundai.com", "Emp_Pwd": "1717", "Emp_Phone": "010-5555-1017", "Position": "차장", "Branch_ID": "'B03'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E018', '{"Emp_Name": "최윤서", "Email_Address": "employee18@hyundai.com", "Emp_Pwd": "1818", "Emp_Phone": "010-5555-1018", "Position": "대리", "Branch_ID": "'B03'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E019', '{"Emp_Name": "이하영", "Email_Address": "employee19@hyundai.com", "Emp_Pwd": "1919", "Emp_Phone": "010-5555-1019", "Position": "대리", "Branch_ID": "'B03'"}');
+INSERT INTO Employee (Emp_ID, jsonstr) VALUES ('E020', '{"Emp_Name": "박성호", "Email_Address": "employee20@hyundai.com", "Emp_Pwd": "2020", "Emp_Phone": "010-5555-1020", "Position": "사원", "Branch_ID": "'B03'"}');
+
+
+
