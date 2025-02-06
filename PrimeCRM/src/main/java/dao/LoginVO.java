7aac3af275451e2113af3e15dd5a84e96e7f33df
suckm <suckm@DESKTOP-DUMRNUO>
2025-02-06 15:13:12 +0900

    login

--- PrimeCRM/src/main/java/dao/LoginVO.java
+++ PrimeCRM/src/main/java/dao/LoginVO.java
@@ -1,14 +1,16 @@
 package dao;
 
 public class LoginVO {
-	private String email, pwd, id;
+	private String name, email, pwd, id;
 	
-	public LoginVO(String email, String pwd, String id) {
+	public LoginVO(String name, String email, String pwd, String id) {
+		this.name=name;
 		this.email=email;
 		this.pwd = pwd;
 		this.id = id;
 	}
 	
+	public String getCName() {return this.name;}
 	public String getCEmail() {return this.email;}
 	public String getCPwd() {return this.pwd;}
 	public String getCId() {return this.id;}
